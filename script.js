0dbbb038e56a8afba6b66c63ae668f0a0491892d
Anwar <anwar@thebrinkagency.com>
2023-04-24 19:12:20 +0200

    60% done

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 let product = [
-    { product: 'PMC', reason: [] },
-    { product: 'HS', reason: [] },
-    { product: 'CHOL', reason: [] },
-    { product: 'AAOX', reason: [] },
-    { product: 'OM3', reason: [] },
-    { product: 'VEG OM3', reason: [] },
-    { product: 'IJzer', reason: [] },
-    { product: 'B12', reason: [] },
-    { product: 'MAG', reason: [] },
-    { product: 'CBD', reason: [] },
-    { product: 'BUF C', reason: [] },
-    { product: 'LS', reason: [] },
-    { product: 'D75', reason: [] },
-    { product: 'D25', reason: [] },
-    { product: 'K2', reason: [] },
-    { product: 'BS', reason: [] },
-    { product: 'HPS', reason: [] },
-    { product: 'JS', reason: [] },
-    { product: 'FIBER', reason: [] },
-    { product: 'MENO', reason: [] },
-    { product: 'BRAIN', reason: [] },
-    { product: 'US', reason: [] },
-    { product: 'IFC', reason: [] },
-    { product: 'SR', reason: [] },
+    { product: 'PMC', reason: [], value: 10 },
+    { product: 'HS', reason: [], value: 1 },
+    { product: 'CHOL', reason: [], value: 1 },
+    { product: 'AAOX', reason: [], value: 1 },
+    { product: 'OM3', reason: [], value: 1 },
+    { product: 'VEG OM3', reason: [], value: 1 },
+    { product: 'IJzer', reason: [], value: 1 },
+    { product: 'B12', reason: [], value: 5 },
+    { product: 'MAG', reason: [], value: 1 },
+    { product: 'CBD', reason: [], value: 1 },
+    { product: 'BUF C', reason: [], value: 1 },
+    { product: 'LS', reason: [], value: 1 },
+    { product: 'D75', reason: [], value: 1 },
+    { product: 'D25', reason: [], value: 1 },
+    { product: 'K2', reason: [], value: 1 },
+    { product: 'BS', reason: [], value: 1 },
+    { product: 'HPS', reason: [], value: 10 },
+    { product: 'JS', reason: [], value: 1 },
+    { product: 'FIBER', reason: [], value: 1 },
+    { product: 'MENO', reason: [], value: 1 },
+    { product: 'BRAIN', reason: [], value: 1 },
+    { product: 'US', reason: [], value: 1 },
+    { product: 'IFC', reason: [], value: 1 },
+    { product: 'SR', reason: [], value: 1 },
+    { product: 'PS', reason: [], value: 1 },
+    { product: 'BC', reason: [], value: 1 },
 ];
 
 function calculateBMI(length, weight) {
@@ -55,12 +57,19 @@ function clearReasons(productArray) {
     });
 }
 
+function sortProductsByValue(products) {
+    return products.sort((a, b) => b.value - a.value);
+}
+
 function displayProductsWithReasons(productArray) {
     let resultsDiv = document.querySelector('.results');
     resultsDiv.innerHTML = '';
+    let counter = 0;
 
     productArray.forEach((item) => {
-        if (item.reason.length > 0) {
+        if (item.reason.length > 0 && counter < 6) {
+            // Show only 6 products
+            // counter++;
             let productTitle = document.createElement('h3');
             productTitle.textContent = item.product;
             resultsDiv.appendChild(productTitle);
@@ -100,8 +109,13 @@ document.querySelector('form').addEventListener('submit', function (event) {
     let fish = parseInt(document.getElementById('vis').value);
     let vegetable = parseInt(document.getElementById('groente').value);
     let processed = document.getElementById('bewerkt').value;
+
     let sportRange = parseInt(document.getElementById('sport-range').value);
-    let sportType = document.querySelector('input[name="sport"]:checked').value;
+    let sportType;
+
+    if (sportRange != 0) {
+        sportType = document.querySelector('input[name="sport"]:checked').value;
+    }
 
     let sun = parseInt(document.getElementById('zon').value);
     let sleep = parseInt(document.getElementById('slaap').value);
@@ -132,10 +146,11 @@ document.querySelector('form').addEventListener('submit', function (event) {
         addReason('AAOX', 'BMI > 25');
     }
 
-    if (skinType == 'dark') {
+    if (skinType == 'donker') {
         addReason('D75', 'Donkere huid');
+    } else if (skinType == 'getint') {
+        addReason('D75', 'Getinte huid');
     }
-
     if (sex == 'vrouw' && age > 50) {
         addReason('BS', 'Vrouw ouder dan 50');
     }
@@ -155,9 +170,11 @@ document.querySelector('form').addEventListener('submit', function (event) {
 
     if (vegetable < 400) {
         addReason('AAOX', 'Minder dan 400 gram groenten per dag');
+        addReason('BC', 'Minder dan 400 gram groenten per dag');
+        addReason('PMC', 'Minder dan 400 gram groenten per dag');
     }
 
-    if (processed > 0) {
+    if (processed == 'regelmatig' || processed == 'vaak') {
         addReason('AAOX', 'Bewerkte voeding');
     }
 
@@ -181,22 +198,34 @@ document.querySelector('form').addEventListener('submit', function (event) {
 
     if (stress == 'ja-stress') {
         addReason('SR', 'Je ervaart stress');
+        addReason('MAG', 'Je ervaart stress');
     }
 
     if (energy == 'nooit' || energy == 'soms') {
         addReason('SR', 'Nooit of Soms voel jij je energiek');
+        addReason('B12', 'Nooit of Soms voel jij je energiek');
+        addReason('BC', 'Nooit of Soms voel jij je energiek');
+        addReason('PMC', 'Nooit of Soms voel jij je energiek');
     }
 
     if (smoke == 'ja-roken') {
         addReason('BUF C', 'Roken');
     }
 
-    if (alchohol > 2) {
-        addReason('LS', 'Meer dan 2 alcoholische dranken per week');
+    if (alchohol > 3 && sex == 'vrouw') {
+        addReason('LS', 'Meer dan 3 alcoholische dranken per week');
+    } else if (alchohol > 6 && sex == 'man') {
+        addReason('LS', 'Meer dan 6 alcoholische dranken per week');
     }
-
     if (drugs == 'ja-drugs') {
         addReason('AAOX', 'Drugsgebruik');
+    }
+
+    if (medicine == 'ja-medicijnen') {
+        addReason('AAOX', 'Medicijnen');
+        alert(
+            'Overleg bij medicatie gebruik altijd eerst voordat u (nieuwe) supplementen gaat gebruiken'
+        );
     }
 
     if (antibiotics == 'ja-antibiotica') {
@@ -213,6 +242,7 @@ document.querySelector('form').addEventListener('submit', function (event) {
 
     if (extra.includes('botten')) {
         addReason('BS', 'Botten');
+        addReason('K2', 'Botten');
     }
 
     if (extra.includes('gewrichten')) {
@@ -229,6 +259,7 @@ document.querySelector('form').addEventListener('submit', function (event) {
 
     if (extra.includes('plasproblemen') && sex == 'man') {
         addReason('PS', 'Plasproblemen bij mannen');
+        addReason('PS', 'Plasproblemen bij mannen');
     }
 
     if (extra.includes('plasproblemen') && sex == 'vrouw') {
@@ -236,7 +267,16 @@ document.querySelector('form').addEventListener('submit', function (event) {
     }
 
     if (extra.includes('nhh')) {
-        addReason('B active complex', 'NHH');
+        addReason('BC', 'NHH');
+        addReason('PMC', 'NHH');
+        addReason('BUF C', 'NHH');
+        addReason('HPS', 'NHH');
+    }
+
+    if (extra.includes('nhh') && diet == 'anders') {
+        addReason('OM3', 'NHH');
+    } else if (extra.includes('nhh') && !diet == 'anders') {
+        addReason('VEG OM3', 'NHH');
     }
 
     if (extra.includes('dss')) {
@@ -270,7 +310,137 @@ document.querySelector('form').addEventListener('submit', function (event) {
     console.log(supplements);
     console.log(extra);
 
-    console.log(product);
+    let sorted = sortProductsByValue(product);
 
-    displayProductsWithReasons(product);
+    displayProductsWithReasons(sorted);
 });
+
+// let subjects = [...document.querySelectorAll('.tba-quiz form > fieldset')];
+
+// for (let i = 0; i < subjects.length; i++) {
+//     let groups = subjects[i].children;
+//     let fieldsetChildren = [];
+
+//     for (let j = 0; j < groups.length; j++) {
+//         if (groups[j].tagName.toLowerCase() === 'fieldset') {
+//             fieldsetChildren.push(groups[j]);
+//         }
+//     }
+// }
+
+let intro = document.querySelector('.tba-introduction');
+let stepProfile = document.querySelector('.step-profile');
+let stepFood = document.querySelector('.step-food');
+let stepLifestyle = document.querySelector('.step-lifestyle');
+let stepSupport = document.querySelector('.step-support');
+let stepFinal = document.querySelector('.step-final');
+let results = document.querySelector('.tba-results');
+
+console.log(stepProfile);
+
+let stepProfileItems = [...stepProfile.querySelectorAll('.step-item')];
+let stepFoodItems = [...stepFood.querySelectorAll('.step-item')];
+let stepLifestyleItems = [...stepLifestyle.querySelectorAll('.step-item')];
+let stepSupportItems = [...stepSupport.querySelectorAll('.step-item')];
+
+let allSteps = [
+    { step: intro, item: [] },
+    { step: stepProfile, item: stepProfileItems },
+    { step: stepFood, item: stepFoodItems },
+    { step: stepLifestyle, item: stepLifestyleItems },
+    { step: stepSupport, item: stepSupportItems },
+    { step: stepFinal, item: [] },
+    { step: results, item: [] },
+];
+
+let next = document.querySelector('.next-button');
+let prev = document.querySelector('.prev-button');
+
+let step = 0;
+let item = 0;
+let start = false;
+
+next.addEventListener('click', handleNext);
+prev.addEventListener('click', handlePrev);
+
+function handleNext() {
+    if (item === allSteps[step].item.length) {
+        item = 0;
+        step++;
+    } else {
+        item++;
+    }
+
+    console.log(allSteps[step].item.length);
+
+    allNone();
+    allSteps[step].step.style.display = 'flex';
+    allSteps[step].item[item].style.display = 'flex';
+
+    console.log(step);
+    console.log(allSteps[step].item.length);
+}
+
+function handlePrev() {
+    if (item == 0) {
+        item = allSteps[step - 1].item.length;
+        step--;
+    } else {
+        item--;
+    }
+
+    allNone();
+    allSteps[step].step.style.display = 'flex';
+    allSteps[step].item[item].style.display = 'flex';
+}
+
+function allNone() {
+    allSteps.forEach((element) => {
+        console.log(element);
+        element.step.style.display = 'none';
+
+        element.item.forEach((group) => {
+            group.style.display = 'none';
+        });
+    });
+}
+
+allNone();
+allSteps[0].step.style.display = 'flex';
+
+let rangeContainer = [...document.querySelectorAll('.range-container')];
+
+rangeContainer.forEach((element) => {
+    let input = element.querySelector('input');
+    var output = element.querySelector('output');
+    const max = parseInt(input.getAttribute('max'));
+    const min = parseInt(input.getAttribute('min'));
+
+    function updateOutputPosition() {
+        const rangeValue = input.value;
+        output.textContent = rangeValue;
+        const percentage = (rangeValue - min) / (max - min);
+        const outputWidth = output.clientWidth;
+
+        const offset = lerp(20, -20, percentage);
+
+        output.style.left = `calc(${percentage * 100}% - (${
+            outputWidth / 2
+        }px) + ${offset}px)`;
+
+        console.log('ofset' + offset);
+    }
+
+    input.addEventListener('input', (event) => {
+        updateOutputPosition();
+    });
+
+    // Initial positioning of output
+    updateOutputPosition();
+
+    output.textContent = 'SCHUIF';
+});
+
+function lerp(start, end, percentage) {
+    return start + (end - start) * percentage;
+}
